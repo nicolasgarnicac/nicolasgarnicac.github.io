23899d154253118789237f2a3365139798b2588a
Nicolasgarnica98 <p.nicolasgarnica@outlook.com>
2023-04-04 20:15:10 -0400

    tab view added

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,3 +115,21 @@
 			});
 
 })(jQuery);
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  window.addEventListener('load', function() {
+	document.getElementById('btn_tab1').click();
+  });
