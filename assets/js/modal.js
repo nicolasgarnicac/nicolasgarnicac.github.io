39504f7365de90a8a1a5b150935fc123a29931c5
Nicolasgarnica98 <p.nicolasgarnica@outlook.com>
2024-01-25 21:58:23 -0500

    pop-up edit #2

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -24,46 +24,28 @@ image.forEach(item => {
 
                     // Assign rigt values based on the Id
                     body.className = "noScroll";
+
+                    modalContainer.className = "modal-container";
+
                     modal.className = "modal";
                     modal.setAttribute("id", project.id);
-                    modal.style.width = "95%";
-                    modal.style.height = "95%";
-                    modal.style.overflow = "hidden";
-                    modal.style.padding = "0px";
-                    modal.style.backgroundColor = "hsl(0, 7%, 97%)";
-                    modalContainer.className = "modal-container";
-                    modal.style.cursor = "default";
 
                     // HEADER
 
                     // Closing button
                     modalBtn.textContent = "×";
-                    modalBtn.style.color = "white";
-                    modalBtn.style.backgroundColor = "rgba(255, 255, 255, 0.0)";
                     modalBtn.className = "close-modal";
                     modalBtn.setAttribute("id", "close-modal");
-                    modalBtn.style.height = "30px";
-                    modalBtn.style.width = "30px";
-                    modalBtn.style.textAlign = "center";
-                    modalBtn.style.borderRadius = "30%";
-                    modalBtn.style.fontSize = "35px"
-                    // modalBtn.style.fontWeight = "bold";
-                    modalBtn.style.border = "none";
+            
 
-                    modalTitle.textContent = project.title;
+                   
                     modalHeader.appendChild(modalTitle);
                     modalHeader.appendChild(modalBtn);
                     modalHeader.className = "modal-header";
-                    modalHeader.style.height = "12%";
-                    modalHeader.style.width = "100%";
-                    modalHeader.style.paddingLeft = '5%';
-                    modalHeader.style.paddingRight = '2%';
-                    // modalHeader.style.paddingBottom = '2%';
-                    modalHeader.style.backgroundImage = "linear-gradient(to left, #244869, #3b6668, #966976)";
-                    modalTitle.style.color = "#d6f3f8";
-                    modalTitle.style.height = "100%";
-                    modalTitle.style.paddingTop = "3%"
-                    modalTitle.style.fontSize = "1.5em";
+
+                    modalTitle.className = "modal-title"
+                    modalTitle.textContent = project.title;
+
 
                     // CONTENT
                     modalContent.style.width = "100%";
