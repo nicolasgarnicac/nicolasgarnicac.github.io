ec1826583770c5b4a543f7f9b4a0f27c1b4d4746
Nicolasgarnica98 <p.nicolasgarnica@outlook.com>
2025-03-07 15:19:22 -0500

    1st Update 2025

--- assets/js/popup_script.js
+++ assets/js/popup_script.js
@@ -1,14 +1,39 @@
 // script.js
 
+const idtourl ={
+  "1":"Projects/Chest X-Ray Pneumonia Classification/chest_xray_classification.html",
+  "2":"Projects/Bone Marrow Cell Classification/bone_marrow_cell_classification.html",
+  "3":"Projects/Bone Marrow Cell Classification/bone_marrow_cell_classification.html",
+  "4":"Projects/Bone Marrow Cell Classification/bone_marrow_cell_classification.html",
+  "5":"Projects/Bone Marrow Cell Classification/bone_marrow_cell_classification.html",
+  "6":"Projects/Bone Marrow Cell Classification/bone_marrow_cell_classification.html",
+  "7":"Projects/resume/resume.html"
+}
+
 // Select elements
 const popup = document.getElementById('popup');
-const openPopup = document.getElementById('1');
+// const openPopup = document.getElementById('1');
+const iframe = document.getElementById('popup-iframe');
+const popup_content = document.getElementById('popup-content');
 const closePopup = document.getElementById('close-popup');
 
-// Show the pop-up
-openPopup.addEventListener('click', () => {
-  popup.style.display = 'flex'; // Make pop-up visible
+// Iterate over the ID-to-URL map to add event listeners
+Object.keys(idtourl).forEach(id => {
+  const element = document.getElementById(id);
+  if (element) {
+    element.addEventListener('click', (e) => {
+      e.preventDefault(); // Prevent default action for links
+      const url = idtourl[id]; // Get URL from the map
+      iframe.src = url; // Update iframe content
+      popup.style.display = 'flex'; // Show the popup
+    });
+  }
 });
+
+// // Show the pop-up
+// openPopup.addEventListener('click', () => {
+//   popup.style.display = 'flex'; // Make pop-up visible
+// });
 
 // Hide the pop-up
 closePopup.addEventListener('click', () => {
@@ -19,5 +44,6 @@ closePopup.addEventListener('click', () => {
 popup.addEventListener('click', (e) => {
   if (e.target === popup) {
     popup.style.display = 'none';
+    iframe.src = ''; // Clear iframe content
   }
 });
